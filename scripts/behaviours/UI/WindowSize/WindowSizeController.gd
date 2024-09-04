f87154c8acadca80054195a5bedfb58630375e6d
Gabriel de Souza <9301442+gabrieldevsouza@users.noreply.github.com>
2024-09-03 22:30:55 -0300

    Unified Window size Anchors script

--- scripts/behaviours/UI/WindowSize/WindowSizeController.gd
+++ scripts/behaviours/UI/WindowSize/WindowSizeController.gd
@@ -11,17 +11,20 @@ enum AnchorType {LEFT, RIGHT, TOP, BOTTOM, TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOT
 var target_position : Vector2 = Vector2.ZERO
 var target_size : Vector2 = Vector2.ZERO
 
-@export var anchor_type : AnchorType = AnchorType.TOP_LEFT
+@export var anchor_type : AnchorType = AnchorType.LEFT
 
 func _ready() -> void:
 	target_size = DisplayServer.window_get_size()
 	target_position = DisplayServer.window_get_position()
+
+	gui_input.connect(_on_gui_input)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 		following = false
 
 func _on_gui_input(event:InputEvent) -> void:
+	print (event)
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.is_pressed():
@@ -37,21 +40,20 @@ func _process(_delta: float) -> void:
 	if following:
 		target_size = DisplayServer.window_get_size()
 		target_position = DisplayServer.window_get_position()
+		
+		if anchor_type == AnchorType.LEFT or anchor_type == AnchorType.TOP_LEFT or anchor_type == AnchorType.BOTTOM_LEFT:
+			target_position.x = DisplayServer.window_get_position().x  + get_global_mouse_position().x - mouse_offset.x
+			target_size.x = window_start_size.x - (DisplayServer.window_get_position().x - window_start_drag_position.x)
 
-		match anchor_type:
-			AnchorType.LEFT:
-				target_position.x = DisplayServer.window_get_position().x  + get_global_mouse_position().x - mouse_offset.x
-				target_size.x = window_start_size.x - (DisplayServer.window_get_position().x - window_start_drag_position.x)
+		if anchor_type == AnchorType.RIGHT or anchor_type == AnchorType.TOP_RIGHT or anchor_type == AnchorType.BOTTOM_RIGHT:
+			target_size.x = get_global_mouse_position().x
 
-			AnchorType.RIGHT:
-				target_position.x = get_global_mouse_position().x
+		if anchor_type == AnchorType.TOP or anchor_type == AnchorType.TOP_LEFT or anchor_type == AnchorType.TOP_RIGHT:
+			target_position.y = DisplayServer.window_get_position().y + get_global_mouse_position().y - mouse_offset.y
+			target_size.y = window_start_size.y - (DisplayServer.window_get_position().y - window_start_drag_position.y)
 
-			AnchorType.TOP:
-				target_position.y = DisplayServer.window_get_position().y + get_global_mouse_position().y - mouse_offset.y
-				target_size.y = window_start_size.y - (DisplayServer.window_get_position().y - window_start_drag_position.y)
-
-			AnchorType.BOTTOM:
-				target_position.y = get_global_mouse_position().y
+		if anchor_type == AnchorType.BOTTOM or anchor_type == AnchorType.BOTTOM_LEFT or anchor_type == AnchorType.BOTTOM_RIGHT:
+			target_size.y = get_global_mouse_position().y
 
 		DisplayServer.window_set_position(target_position)
 		DisplayServer.window_set_size(target_size)
