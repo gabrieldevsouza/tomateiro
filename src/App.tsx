22c568e4442194cc19bdc62ad24e91b4798d283c
Gabriel de Souza <9301442+gabrieldevsouza@users.noreply.github.com>
2026-03-23 23:12:06 -0300

    Body style improvement and default system title bar removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <main className="app-root" aria-label="Tomateiro app root" />;
+  return (
+    <main className="app-root" aria-label="Tomateiro app root">
+      <p className="text-base-content">Hello, world. Welcome to Tomateiro.</p>
+    </main>
+  );
 }
 
 export default App;
