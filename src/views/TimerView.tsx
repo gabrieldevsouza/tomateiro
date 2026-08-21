4018fe316099f5342cc2c62c8621b769e199ae44
Gabriel de Souza <9301442+gabrieldevsouza@users.noreply.github.com>
2026-08-21 11:44:17 -0300

    Refactor TimerView layout, add container query
    Replace the simple centered heading with a responsive container-based layout. The section now declares [container-type:size] and contains an inner responsive block (aspect-[8/9], w-[min(100cqw,88.8889cqh)], flex-col) with adjusted colors. Removed the previous border and static h1 to prepare for a richer, responsive timer UI.

--- src/views/TimerView.tsx
+++ src/views/TimerView.tsx
@@ -1,21 +1,30 @@
 function TimerView() {
 	return (
-		<section className="
-			flex
-			h-full
-			w-full
-			items-center
-			justify-center
-			border-4
-			border-green-600
-			bg-orange-300
-			text-orange-950
-		">
-			<div className="text-center">
-				<h1 className="text-2xl font-semibold">
-					TimerView
-				</h1>
-			</div>
+		<section
+			className="
+				flex
+				h-full
+				w-full
+				items-center
+				justify-center
+				bg-orange-300
+				text-orange-950
+				[container-type:size]
+			"
+		>
+			<div
+				className="
+					flex
+					aspect-[8/9]
+					w-[min(100cqw,88.8889cqh)]
+					flex-col
+					items-center
+					justify-center
+					bg-red-400
+					text-center
+					text-orange-950
+				"
+			/>
 		</section>
 	);
 }
