b0cccecec4dc93aea6413905f630ae02864a9810
Gabriel de Souza <9301442+gabrieldevsouza@users.noreply.github.com>
2026-08-21 12:35:56 -0300

    Update PomodoroView.tsx

--- src/views/PomodoroView.tsx
+++ src/views/PomodoroView.tsx
@@ -9,69 +9,33 @@ function PomodoroView() {
 				w-full
 				items-center
 				justify-center
-				[container-type:size]
+				@container-size
 			"
 		>
 			<div
 				className="
-					flex
-					aspect-[8/9]
+					grid
+					aspect-8/9
 					w-[min(100cqw,88.8889cqh)]
-					flex-row
-					items-center
-					justify-center
+					grid-cols-[3fr_10fr_3fr]
 				"
 			>
 				<div className="
-					flex
-					flex-[3_1_0%]
+					col-start-2
+					grid
 					h-full
-					items-center
-					justify-center
-				" />
-
-				<div className="
-					flex
-					flex-col
-					flex-[10_1_0%]
-					h-full
-					items-center
-					justify-center
+					grid-rows-[1fr_4fr_1fr]
 				">
-					<div className="
-						flex
-						flex-[1_1_0%]
-						w-full
-						items-center
-						justify-center
-					" />
 
 					<div className="
+						row-start-2
 						flex
-						flex-[4_1_0%]
-						w-full
 						items-center
 						justify-center
 					">
 						<PomodoroPanel />
 					</div>
-
-					<div className="
-						flex
-						flex-[1_1_0%]
-						w-full
-						items-center
-						justify-center
-					" />
 				</div>
-
-				<div className="
-					flex
-					flex-[3_1_0%]
-					h-full
-					items-center
-					justify-center
-				" />
 			</div>
 		</section>
 	);
