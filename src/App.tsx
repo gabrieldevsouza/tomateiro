eb91135729c93aa727228b2c23628e5cceea995e
Carol Faustino <165836292+AnaC-F@users.noreply.github.com>
2026-09-23 20:30:29 -0300

    Enable Pomodoro view in app
    The app was still rendering the selection screen instead of the actual timer experience. This change re-enables PomodoroView in the main app entry and comments out the selection view so the application loads the intended Pomodoro flow.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import AppShell from "./layout/AppShell";
-/*import PomodoroView from "./views/PomodoroView";*/
-import SelectionView from "./views/SelectionView";
+import PomodoroView from "./views/PomodoroView";
+/*import SelectionView from "./views/SelectionView";*/
 
 function App() {
 	return (
 		<AppShell>
 			{/*<PomodoroView />*/}
-			<SelectionView />
+			<PomodoroView />
 		</AppShell>
 	);
 }
